cc56f0497c1cbfb9e4ada067e0eddea9607409b8
Basim415 <bshahzad@horizon.csueastbay.edu>
2025-04-11 19:35:42 -0700

    Completed 2 more functions. Still need to work on formatting

--- popup.js
+++ popup.js
@@ -1,11 +1,78 @@
-const addNewBookmark = () => {};
+import { getActiveTabURL } from "./utils.js";
 
-const viewBookmarks = () => {};
+const addNewBookmark = (bookmarksElement, bookmark) => {
+    const bookmarkTitleElement = document.createElement("div");
+    const newBookmarkElement = document.createElement("div");
+    const controlsElement = document.createElement("div");
 
-const onPlay = e => {};
 
-const onDelete = e => {};
+    bookmarkTitleElement.textContent = bookmark.desc;
+    bookmarkTitleElement.className = "bookmark-title";
 
-const setBookmarkAttributes = () => {};
+    controlsElement.className = "bookmark-controls";
 
-document.addEventListener("DOMContentLoaded", () => {});
+    newBookmarkElement.id = "bookmark-" + bookmark.time;
+    newBookmarkElement.className = "bookmark";
+    newBookmarkElement.setAttribute("timestamp", bookmark.time);
+
+    setBookmarkAttributes("play", onPlay, controlsElement);
+
+    newBookmarkElement.appendChild(bookmarkTitleElement);
+    newBookmarkElement.appendChild(controlsElement);
+    bookmarksElement.appendChild(newBookmarkElement);
+};
+
+const viewBookmarks = (currentBookmarks = []) => {
+    const bookmarksElement = document.getElementById("bookmarks");
+    bookmarksElement.innerHTML = "";
+
+    if (currentBookmarks.length > 0) {
+        for (let i = 0; i < currentBookmarks.length; i++) {
+            const bookmark = currentBookmarks[i];
+            addNewBookmark(bookmarksElement, bookmark);
+        }
+    } else {
+        bookmarksElement.innerHTML = '<i class="row">No bookmarks to show</i>'; // fixed HTML error: unmatched quote
+    }
+};
+
+const onPlay = (e) => {
+    const bookmarkTime = e.target.parentNode.getAttribute("timestamp");
+    const activeTab = await 
+    // To be implemented
+};
+
+const onDelete = (e) => {
+    // To be implemented
+};
+
+const setBookmarkAttributes = (src, eventListener, controlParentElement) => {
+    const controlElement = document.createElement("img");
+
+    controlElement.src = "assests/" + src + ".png";
+    controlElement.title = src;
+    controlElement.addEventListener("click", eventListener);
+    controlParentElement.appendChild(controlElement);
+    
+};
+
+document.addEventListener("DOMContentLoaded", async () => {
+    const activeTab = await getActiveTabURL();
+
+    const queryParameters = activeTab.url.split("?")[1];
+    const urlParameters = new URLSearchParams(queryParameters);
+
+    const currentVideo = urlParameters.get("v");
+
+    if (activeTab.url.includes("youtube.com/watch") && currentVideo) {
+        chrome.storage.sync.get([currentVideo], (data) => {
+            const currentVideoBookmarks = data[currentVideo]
+                ? JSON.parse(data[currentVideo])
+                : [];
+            viewBookmarks(currentVideoBookmarks);
+        });
+    } else {
+        const container = document.getElementsByClassName("container")[0];
+        container.innerHTML = '<div class="title">This is not a YouTube video page.</div>';
+    }
+});
